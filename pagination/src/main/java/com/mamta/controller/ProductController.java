3750d4fbcf19620f664378e307b70a831dcb1a0d
mamata <mamatadewal99@gmail.com>
2025-10-15 18:33:41 +0530

    commit

--- pagination/src/main/java/com/mamta/controller/ProductController.java
+++ pagination/src/main/java/com/mamta/controller/ProductController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mamta.dto.ProductDto;
 import com.mamta.entity.Product;
 import com.mamta.service.ProductService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/products")
@@ -37,12 +40,24 @@ public class ProductController {
 	private ProductDto<Page<Product>> getProductwithPagination(@PathVariable Integer pageNo,@PathVariable Integer pageSize){
 		Page<Product> productWithPagination=service.findProductWithPagination(pageNo, pageSize);
 		return new ProductDto<>(productWithPagination.getSize(),productWithPagination);
+		//instead of getSize(),used getTotalElement()
 	}
 	
 	@GetMapping("paginationwithsort/{offset}/{pageSize}/{field}")
 	private ProductDto<Page<Product>> getProductwithPaginationAndSort(@PathVariable Integer pageNo,@PathVariable Integer pageSize,@PathVariable String field){
 		Page<Product> productWithPaginationAndSort=service.findProductWithPaginationAndSorting(pageNo, pageSize, field);
 				return new ProductDto<>(productWithPaginationAndSort.getSize(),productWithPaginationAndSort);
+	}
+	
+	@GetMapping("/filter")
+	private ProductDto<Page<Product>> getProduct(
+			@RequestParam(defaultValue="0") Integer page,
+			@RequestParam(defaultValue="5") Integer size,
+			@RequestParam(defaultValue="id") String sortBy,
+			@RequestParam(defaultValue="asc") String sortDir
+			){
+		Page<Product> productWithPaginationSortDir=service.findProductWithPaginationSortDir(page, size, sortBy, sortDir);
+		return  new ProductDto<>(productWithPaginationSortDir.getSize(),productWithPaginationSortDir);
 	}
 
 }
